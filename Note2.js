acfbfdc0bb9126e62972dd25d9d90980f2921fb8
Ishita Gupta <65915970+ishitagupta014@users.noreply.github.com>
2021-06-06 22:47:07 +0530

    Add files via upload

---
+++ Note2.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+ const Note2 = () => {
+    return (
+      <>  
+        <div className="note">
+              Here's the link for you to watch the sessions as well<br/>
+            <a href="https://www.youtube.com/playlist?list=PL7zl8TDRnbulLetcbkthT0p_IzwgRAYbu"> <img src="https://github.com/ShapeAI/PYTHON-AND-DATA-ANALYTICS/blob/main/YOUTUBE%20THUMBNAIL-4.png"/> </a>
+            <br/>I got to have hands on experience on:
+            <li/>JavaScript
+            <li/>HTML
+            <li/>React.js
+            <br/>during these 7 days, and everything was explained from the very basics so that
+            anyone with zero experience on programming can learn.
+
+        </div>
+        </>
+    )
+}
+export default Note2
