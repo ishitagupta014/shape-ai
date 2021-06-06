a923719e5f90536c19798b1e9b3096cb230d8174
Ishita Gupta <65915970+ishitagupta014@users.noreply.github.com>
2021-06-06 22:46:41 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+const Footer=()=> {
+  return (
+      <>
+      <footer>
+            <p>
+              I enjoyed these 7 days, you can as well. To register for next free 7 days bootcamp, visit:</p>
+        <a href="https://www.shapeai.tech"> www.shapeai.tech</a>
+        or follow SHAPEAI on:
+        <li/><a href=
+        "https://in.linkedin.com/company/shapeai">LinkedIn</a>
+        <li/><a href=
+        "https://www.instagram.com/shape.ai/?hl=en">Instagram</a>
+        <li/><a
+        href=
+        "https://www.youtube.com/channel/UCTUvDLTW9meuDXWcbmISPdA">YouTu
+        be</a>
+        <li/><a href=
+        "https://github.com/shapeai">GitHub</a>
+
+
+         
+        </footer>
+      </>
+  );
+};
+
+export default Footer;
---
+++ Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+const Header=()=>{
+    return(
+        <>
+         <header>
+         <h1 >SHAPEAI <br></br>Javascript and Reactjs BOOTCAMP
+           </h1>
+         </header>
+        </>
+    )
+}
+export default Header
