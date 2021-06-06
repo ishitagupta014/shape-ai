081dcc9aed900ba4ad60c415b3fb34383396ae8b
Ishita Gupta <65915970+ishitagupta014@users.noreply.github.com>
2021-06-06 22:46:02 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './styles.css';
+import Note from "./Note"
+import Note2 from "./Note2"
+import Header from "./Header"
+import Footer from "./Footer"
+function App() {
+  return (
+        <>
+         <Header></Header>
+         <Note></Note>
+          <Note2></Note2>
+     
+         <Footer></Footer>
+        </>
+  );
+}
+
+export default App;
